12fe04f8ce35e263104bd52cf7130799c4cff74b
Alisa Pavlova <alipavlova2020@gmail.com>
2022-07-14 13:21:55 +0300

    init

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,16 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SQRServiceTest {
+
+    @Test
+    public void shouldCalc() {
+        SQRService service = new SQRService();
+        int expected = 3;
+        int actual = service.calculate(200, 300);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
